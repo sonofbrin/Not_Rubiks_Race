dab4df222919257fd19ffa0c5439fc270e49533e
Stevie <stevie.brinson@gmail.com>
2025-03-19 22:26:26 -0400

    Added keyboard controls for both QWERTY and arrow key schemes.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import './App.css'
 import Tile from './components/Tile'
 
@@ -6,7 +6,86 @@ const BOARD_WIDTH = 5
 const BOARD_HEIGHT = 5
 
 export default function App() {
-  const handleTileClick = (row, col) => {
+  const flipVertical = -1
+  useEffect(() => {
+    const handleKeyDown = ({ key }) => {
+      let rowOffset = 0
+      let colOffset = 0
+      switch (key) {
+        case 'ArrowRight':
+        case 'f':
+          colOffset = -1
+          break
+        case 'd':
+          colOffset = -2
+          break
+        case 's':
+          colOffset = -3
+          break
+        case 'a':
+          colOffset = -4
+          break
+        case 'ArrowLeft':
+        case 'j':
+          colOffset = 1
+          break
+        case 'k':
+          colOffset = 2
+          break
+        case 'l':
+          colOffset = 3
+          break
+        case ';':
+          colOffset = 4
+          break
+        case 'ArrowDown':
+        case 'r':
+        case 'u':
+          rowOffset = 1 * flipVertical
+          break
+        case 'e':
+        case 'i':
+          rowOffset = 2 * flipVertical
+          break
+        case 'w':
+        case 'o':
+          rowOffset = 3 * flipVertical
+          break
+        case 'q':
+        case 'p':
+          rowOffset = 4 * flipVertical
+          break
+        case 'ArrowUp':
+        case 'v':
+        case 'm':
+          rowOffset = -1 * flipVertical
+          break
+        case 'c':
+        case ',':
+          rowOffset = -2 * flipVertical
+          break
+        case 'x':
+        case '.':
+          rowOffset = -3 * flipVertical
+          break
+        case 'z':
+        case '/':
+          rowOffset = -4 * flipVertical
+          break
+        default:
+          return
+      }
+      const targetRow = Math.max(0, Math.min(blank.current[0] + rowOffset, BOARD_HEIGHT - 1))
+      const targetCol = Math.max(0, Math.min(blank.current[1] + colOffset, BOARD_WIDTH - 1))
+      if (targetRow != blank.current[0] || targetCol != blank.current[1]) {
+        slideTilesFromCoordinates(targetRow, targetCol)
+      }
+    }
+    document.addEventListener('keydown', handleKeyDown, true)
+    return () => document.removeEventListener('keydown', handleKeyDown)
+  }, [])
+
+  const slideTilesFromCoordinates = (row, col) => {
     const [blankRow, blankCol] = blank.current
     if (row != blankRow && col != blankCol) return
     if (row == blankRow) {
@@ -25,7 +104,7 @@ export default function App() {
               row={row}
               col={i}
               value={id % 6}
-              handleClick={handleTileClick}
+              handleClick={slideTilesFromCoordinates}
             />
           )
         }
@@ -47,7 +126,7 @@ export default function App() {
               row={i}
               col={col}
               value={id % 6}
-              handleClick={handleTileClick}
+              handleClick={slideTilesFromCoordinates}
             />
           )
         }
@@ -68,7 +147,7 @@ export default function App() {
         row={i}
         col={j}
         value={col % 6}
-        handleClick={handleTileClick}
+        handleClick={slideTilesFromCoordinates}
       />
     )))
   }, []))
